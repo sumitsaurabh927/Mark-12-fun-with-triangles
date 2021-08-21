cc26c8d5d047635136786a35d479749058e59b6d
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-21 11:57:57 +0530

    able to get user submissions

--- quiz.js
+++ quiz.js
@@ -1,6 +1,31 @@
 var btn = document.querySelector("#btn")
+var triangleForm = document.querySelector("#form")
+var formResults = new FormData(triangleForm)
+// console.log(formResults);
 btn.addEventListener("click", btnPressed)
 
+
+const correctAns=["100","equilateral"]
+
+
 function btnPressed() {
-    console.log("working fine")
+    // console.log("working fine")
+
+    var i=0;
+    var score=0;
+
+    for (data of formResults){
+        console.log(data);
+        var userAns=data[1];
+        // if (userAns===correctAns[i]){
+        //     console.log("correct");
+        //     score+=10
+        // }else{
+        //     console.log("incorrect");
+        // }
+        // i+=1
+    console.log(userAns);
+    }
+    console.log("your score is: "+score);
+
 }
