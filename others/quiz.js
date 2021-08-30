e1cfbf825647c3e7518c0b042acd2b65c98ce4c3
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-30 17:39:47 +0530

    fix: score not updating

--- others/quiz.js
+++ others/quiz.js
@@ -1,21 +1,40 @@
 var btn = document.querySelector("#btn")
 var triangleForm = document.querySelector("#form")
-var formResults = new FormData(triangleForm)
-btn.addEventListener("click", btnPressed)
-const correctAns = ["100", "equilateral"]
+// var formResults = new FormData(triangleForm)
+
+
+const correctAns = ["100°", "equilateral"]
 var out = document.querySelector("#output")
+
+
+btn.addEventListener("click", btnPressed)
+
+
+
 
 function btnPressed() {
     var i = 0;
     var score = 0;
 
-    for (let data of formResults.values()) {
+    const formResult = new FormData(triangleForm)
+
+    for (let data of formResult.values()) {
+
+        out.innerText = ""
+        // console.log("clicked");
+
         console.log(data);
-        if (data === correctAns[i]) {
-            console.log("correct");
+        if (data == correctAns[i]) {
+            // console.log("correct");
             score += 10
+            // console.log(score);
+        } else {
+            // console.log("incorrect");
+            score = score + 0;
+            // console.log(score);
         }
         i += 1
     }
     out.innerText = "Final Score: " + score;
+
 }
